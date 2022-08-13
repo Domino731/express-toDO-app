9d3db3286cc36e5c798c45edcacae08c85b9283e
Domino731 <d.orzechowski18@gmail.com>
2022-08-13 16:17:33 +0200

    BE - mongo - init

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
+require('dotenv').config({path: __dirname + '/.env'})
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+const dbUrl = process.env['MONGO_DB'];
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+
 app.listen("3000", () => {
+    console.log(process.env['MONGO_DB']);
     console.log("server started");
 });
