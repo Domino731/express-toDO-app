3cb6240382c4761fe2fd327d90e8cc4bc1ee1d99
Domino731 <d.orzechowski18@gmail.com>
2022-08-17 11:50:15 +0200

    BE - auth - hashing password

---
+++ server/cookies/routes.js
@@ -0,0 +1,5 @@
+const {Router} = require("express");
+// const authController = require("../../controlers/authControler");
+
+// server router
+const router = Router();
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const {isEmail} = require("validator");
+const bcrypt = require("bcrypt");
 
+// mongo schema for user
 const userSchema = mongoose.Schema({
     email: {
         type: String,
@@ -14,6 +16,23 @@ const userSchema = mongoose.Schema({
         minLength: 6,
     }
 });
+
+// fire a function after doc saved to mongoDB
+userSchema.post("save", function (doc, next) {
+    console.log("MONGO: New user was created")
+    next();
+});
+
+// fire a function before doc save
+userSchema.pre("save", async function (next) {
+    // hash the password
+    const salt = await bcrypt.genSalt();
+
+    // "this" refers to user data - password & email
+    this.passwod = await bcrypt.hash(this.password, salt);
+    next();
+})
+
 
 const User = mongoose.model("user", userSchema);
 module.exports = User;
