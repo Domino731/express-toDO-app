98eb47163b7f5f9a4c0f659207901cc2bd744687
Domino731 <d.orzechowski18@gmail.com>
2022-08-31 17:58:26 +0200

    FE - home styles improvements

--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,8 +1,12 @@
 import {NewTaskHeader} from "./Components/NewTaskHeader";
 import {TasksBoard} from "./Components/TasksBoard";
 
-export const Home = () => <div className="h-full w-full bg-white bg-gray-200">
-    <div className='w-[1000px] mx-auto h-auto mt-[80px] shadow-2xl px-6 py-8 rounded-md'>
+export const Home = () => <div className="h-full w-full bg-white bg-gray-200" style={{
+    background: 'linear-gradient(to right, #8e2de2, #4a00e0)',
+    backgroundSize: '100% 400px',
+    backgroundRepeat: 'no-repeat'
+}}>
+    <div className='w-[1000px] mx-auto h-auto mt-[80px] shadow-2xl px-6 py-8 rounded-md relative bg-gray-100'>
         <NewTaskHeader/>
         <TasksBoard/>
     </div>
