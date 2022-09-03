71d94c3efd5531e76d114358adc0dce1d3898ca8
Domino731 <d.orzechowski18@gmail.com>
2022-09-03 18:52:24 +0200

    FE - checkbox component

--- src/Containers/Home/Components/Operation.tsx
+++ src/Containers/Home/Components/Operation.tsx
@@ -1,5 +1,6 @@
 import {FunctionComponent} from "react";
 import {OperationInterface} from "../../../Reducers/user/types";
+import Checkbox from "../../../Components/ Checkbox";
 
 interface OperationProps {
     data: OperationInterface;
@@ -7,6 +8,7 @@ interface OperationProps {
 
 export const Operation: FunctionComponent<OperationProps> = ({data}) => {
     return <div>
+        <Checkbox checked={false}/>
         {data.title}
     </div>
 }
