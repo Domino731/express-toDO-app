a7e93663162779fc2c5d83e8753845f743222b93
Domino731 <d.orzechowski18@gmail.com>
2022-09-11 18:12:46 +0200

    FE - task types

---
+++ src/Containers/Home/const.ts
@@ -0,0 +1,18 @@
+export const taskTypes = [
+    {
+        label: 'TECH',
+        color: 'bg-blue-600'
+    },
+    {
+        label: 'Sport',
+        color: 'bg-fushia-600'
+    },
+    {
+        label: 'Home',
+        color: 'bg-rose-600'
+    },
+    {
+        label: 'Study',
+        color: 'bg-green-600'
+    }
+]
